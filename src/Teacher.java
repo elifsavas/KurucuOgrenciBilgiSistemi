d4927efb939e526ca05e7e6cf2e892c351f82856
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2023-02-02 00:34:40 +0300

    Update Teacher.java

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Teacher {
+public class Teacher {
+    String name;
+    String mpno;
+    String branch;
+
+    Teacher(String name, String branch, String mpno) {
+        this.name = name;
+        this.branch = branch;
+        this.mpno = mpno;
+    }
+
+    void print () {
+        System.out.println("Öğretmen Adı: " + this.name);
+        System.out.println("Branş : " + this.branch);
+        System.out.println("Telefon Numarası : " + this.mpno);
+    }
+
 }
+
