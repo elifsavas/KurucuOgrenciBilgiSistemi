d65dcc9adc3b379d6d35fc24454de0ec20e22efe
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2023-02-02 00:33:00 +0300

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Course {
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        int note = 0 ;
+    }
+
+    void addTeacher (Teacher teacher) {
+        if (teacher.branch.equals(this.prefix)) {
+            this.teacher = teacher ;
+            //printTeacher();
+        } else {
+            System.out.println("Öğretmen ismi ve branş uyuşmuyor!");
+        }
+
+
+    }
+
+    void printTeacher () {
+        this.teacher.print();
+    }
+
 }
+
